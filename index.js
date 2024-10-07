118deaa074369a29de335ac35d093c265e0c2226
Orellana Matias <meorellanaramirez@gmail.com>
2024-10-06 22:08:14 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,84 @@
+const RESPONSIVE_WIDTH = 1024
+
+let headerWhiteBg = false
+let isHeaderCollapsed = window.innerWidth < RESPONSIVE_WIDTH
+const collapseBtn = document.getElementById("collapse-btn")
+const collapseHeaderItems = document.getElementById("collapsed-header-items")
+
+
+
+function onHeaderClickOutside(e) {
+
+    if (!collapseHeaderItems.contains(e.target)) {
+        toggleHeader()
+    }
+
+}
+
+
+function toggleHeader() {
+    if (isHeaderCollapsed) {
+        collapseHeaderItems.classList.add("opacity-100",)
+        collapseHeaderItems.style.width = "60vw"
+        collapseBtn.classList.remove("bi-list")
+        collapseBtn.classList.add("bi-x", "max-lg:tw-fixed")
+        isHeaderCollapsed = false
+
+        setTimeout(() => window.addEventListener("click", onHeaderClickOutside), 1)
+
+    } else {
+        collapseHeaderItems.classList.remove("opacity-100")
+        collapseHeaderItems.style.width = "0vw"
+        collapseBtn.classList.remove("bi-x", "max-lg:tw-fixed")
+        collapseBtn.classList.add("bi-list")
+        isHeaderCollapsed = true
+        window.removeEventListener("click", onHeaderClickOutside)
+
+    }
+}
+
+function responsive() {
+    if (window.innerWidth > RESPONSIVE_WIDTH) {
+        collapseHeaderItems.style.width = ""
+
+    } else {
+        isHeaderCollapsed = true
+    }
+}
+
+window.addEventListener("resize", responsive)
+
+
+/**
+ * Animations
+ */
+
+gsap.registerPlugin(ScrollTrigger)
+
+
+const numberTimeline = gsap.timeline({paused: true, scrollTrigger: {
+    trigger: "#numbers",
+    start: "100% 100%",
+    end: "100% 90%",
+}
+})
+
+numberTimeline.fromTo("#numbers-container", {
+    scale: 0.8,
+}, {
+
+    scale: 1,
+    duration: 3
+}).to("#installs", {
+    innerText: 10,
+    duration: 3,
+    snap: {
+        innerText: 1
+    },
+}, "<").to("#hours", {
+    innerText: 10,
+    duration: 3,
+    snap: {
+        innerText: 1
+    }
+}, "<")
